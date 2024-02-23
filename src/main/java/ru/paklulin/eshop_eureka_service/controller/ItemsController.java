1a1433a2722fe02727d6b17a92309b99a652eba6
lewe0fun <pakuln.yury@gmail.com>
2024-02-23 16:56:52 +0300

    actuator item-service

--- src/main/java/ru/paklulin/eshop_eureka_service/controller/ItemsController.java
+++ src/main/java/ru/paklulin/eshop_eureka_service/controller/ItemsController.java
@@ -1,5 +1,6 @@
 package ru.paklulin.eshop_eureka_service.controller;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.paklulin.eshop_eureka_service.models.Item;
@@ -11,24 +12,29 @@ import java.util.List;
 @RequestMapping("/items/")
 public class ItemsController {
     private final ItemService itemService;
+    private final MeterRegistry meterRegistry;
 
     @Autowired
-    public ItemsController(ItemService itemService) {
+    public ItemsController(ItemService itemService, MeterRegistry meterRegistry) {
         this.itemService = itemService;
+        this.meterRegistry = meterRegistry;
     }
 
     @GetMapping()
     public List<Item> getAllItems() {
+        meterRegistry.counter("getAllItems").increment();
         return itemService.findAll();
     }
 
     @GetMapping("/{id}")
     public Item getItem(@PathVariable("id") int id) {
+        meterRegistry.counter("get "+id).increment();
         return itemService.findById(id);
     }
 
     @PostMapping()
     public Item newItem(@RequestBody Item item) {
+        meterRegistry.counter("add new item").increment();
         return itemService.save(item);
     }
 
